2de1cff5141bd9d9d390857e369c159147e528f1
Filipe Peliz Pinto Teixeira <filipe@takealot.com>
2018-11-27 17:20:14 +0200

    Check

--- src/test/java/com/github/fppt/jedismock/comparisontests/ComparisonBase.java
+++ src/test/java/com/github/fppt/jedismock/comparisontests/ComparisonBase.java
@@ -41,10 +41,10 @@ public class ComparisonBase {
         Jedis[] jedis = new Jedis[2];
 
         //Create real jedis connection
-        jedis[0] = new Jedis("localhost", EmbeddedRedis.PORT);
+        jedis[1] = new Jedis("localhost", EmbeddedRedis.PORT);
 
         //Create mocked jedis connection
-        jedis[1] = new Jedis(fakeServer.getHost(), fakeServer.getBindPort(), 1000000);
+        jedis[0] = new Jedis(fakeServer.getHost(), fakeServer.getBindPort(), 1000000);
 
         return jedis;
     }
