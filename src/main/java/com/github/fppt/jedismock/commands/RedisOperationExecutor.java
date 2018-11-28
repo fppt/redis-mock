4d0f4b54095126c30a253416938d6c9239b4ba64
Filipe Peliz Pinto Teixeira <filipe@takealot.com>
2018-11-28 09:13:51 +0200

    Get rid of reflection based op builder

--- src/main/java/com/github/fppt/jedismock/commands/RedisOperationExecutor.java
+++ src/main/java/com/github/fppt/jedismock/commands/RedisOperationExecutor.java
@@ -9,11 +9,8 @@ import com.github.fppt.jedismock.exception.WrongValueTypeException;
 import com.google.common.base.Preconditions;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by Xiaolu on 2015/4/20.
@@ -32,33 +29,115 @@ public class RedisOperationExecutor {
         transaction = new ArrayList<>();
     }
 
-    private RedisOperation buildRedisOperation(String name, List<Slice> params){
-        try {
-            String className = this.getClass().getPackage().getName() + ".RO_" + name;
-            Class<?> clazz = Class.forName(className);
-            @SuppressWarnings("unchecked")
-            Constructor<RedisOperation> constructor = (Constructor<RedisOperation>) clazz.getDeclaredConstructors()[0];
-            return constructor.newInstance(base, params);
-        } catch (ClassNotFoundException e) {
-            throw new UnsupportedOperationException(String.format("Unsupported operation '%s'", name));
-        } catch (IllegalAccessException | InstantiationException | InvocationTargetException | IllegalArgumentException e) {
-            throw new UnsupportedOperationException(String.format("Error initialising operation '%s'", name));
-        }
-    }
-
-    private Optional<RedisOperation> buildMetaOperation(String name, List<Slice> params){
+    private RedisOperation buildSimpleOperation(String name, List<Slice> params){
         switch(name){
+            case "set":
+                return new RO_set(base, params);
+            case "setex":
+                return new RO_setex(base, params);
+            case "psetex":
+                return new RO_psetex(base, params);
+            case "setnx":
+                return new RO_setnx(base, params);
+            case "setbit":
+                return new RO_setbit(base, params);
+            case "append":
+                return new RO_append(base, params);
+            case "get":
+                return new RO_get(base, params);
+            case "getbit":
+                return new RO_getbit(base, params);
+            case "ttl":
+                return new RO_ttl(base, params);
+            case "pttl":
+                return new RO_pttl(base, params);
+            case "expire":
+                return new RO_expire(base, params);
+            case "pexpire":
+                return new RO_pexpire(base, params);
+            case "incr":
+                return new RO_incr(base, params);
+            case "incrby":
+                return new RO_incrby(base, params);
+            case "decr":
+                return new RO_decr(base, params);
+            case "decrby":
+                return new RO_decrby(base, params);
+            case "pfcount":
+                return new RO_pfcount(base, params);
+            case "pfadd":
+                return new RO_pfadd(base, params);
+            case "pfmerge":
+                return new RO_pfmerge(base, params);
+            case "mget":
+                return new RO_mget(base, params);
+            case "mset":
+                return new RO_mset(base, params);
+            case "getset":
+                return new RO_getset(base, params);
+            case "strlen":
+                return new RO_strlen(base, params);
+            case "del":
+                return new RO_del(base, params);
+            case "exists":
+                return new RO_exists(base, params);
+            case "expireat":
+                return new RO_expireat(base, params);
+            case "pexpireat":
+                return new RO_pexpireat(base, params);
+            case "lpush":
+                return new RO_lpush(base, params);
+            case "rpush":
+                return new RO_rpush(base, params);
+            case "lpushx":
+                return new RO_lpushx(base, params);
+            case "lrange":
+                return new RO_lrange(base, params);
+            case "llen":
+                return new RO_llen(base, params);
+            case "lpop":
+                return new RO_lpop(base, params);
+            case "rpop":
+                return new RO_rpop(base, params);
+            case "lindex":
+                return new RO_lindex(base, params);
+            case "rpoplpush":
+                return new RO_rpoplpush(base, params);
+            case "brpoplpush":
+                return new RO_brpoplpush(base, params);
+            case "subscribe":
+                return new RO_subscribe(base, owner, params);
+            case "unsubscribe":
+                return new RO_unsubscribe(base, owner, params);
+            case "publish":
+                return new RO_publish(base, params);
+            case "flushall":
+                return new RO_flushall(base, params);
+            case "lrem":
+                return new RO_lrem(base, params);
             case "quit":
-                return Optional.of(new RO_quit(base, owner, params));
+                return new RO_quit(base, owner, params);
             case "exec":
                 transactionModeOn = false;
-                return Optional.of(new RO_exec(base, transaction, params));
-            case "subscribe":
-                return Optional.of(new RO_subscribe(base, owner, params));
-            case "unsubscribe":
-                return Optional.of(new RO_unsubscribe(base, owner, params));
+                return new RO_exec(base, transaction, params);
+            case "ping":
+                return new RO_ping(base, params);
+            case "keys":
+                return new RO_keys(base, params);
+            case "sadd":
+                return new RO_sadd(base, params);
+            case "smembers":
+                return new RO_smembers(base, params);
+            case "spop":
+                return new RO_spop(base, params);
+            case "hget":
+                return new RO_hget(base, params);
+            case "hset":
+                return new RO_hset(base, params);
+            case "hdel":
+                return new RO_hdel(base, params);
             default:
-                return Optional.empty();
+                throw new UnsupportedOperationException(String.format("Unsupported operation '%s'", name));
         }
     }
 
@@ -75,10 +154,8 @@ public class RedisOperationExecutor {
                 return Response.clientResponse(name, Response.OK);
             }
 
-            RedisOperation redisOperation = buildMetaOperation(name, commandParams).
-                    orElseGet(() -> buildRedisOperation(name, commandParams));
-
             //Checking if we mutating the transaction or the base
+            RedisOperation redisOperation = buildSimpleOperation(name, commandParams);
             if(transactionModeOn){
                 transaction.add(redisOperation);
             } else {
